f3e25effd2e72c8df4192edee582e16896c7c231
Jean Malm <jean.malm@mdh.se>
2021-03-22 17:04:06 +0100

    Timers can now also be used in 'lap' mode, e.g. combine a number of individual samplings

--- TimerWrapper.cpp
+++ TimerWrapper.cpp
@@ -1,8 +1,9 @@
 #include "TimerWrapper.h"
 
-    TimerWrapper::TimerWrapper(const std::string& event) : event_name(event)
+    TimerWrapper::TimerWrapper(const std::string& event, size_t default_event_amount) : event_name(event)
     {
-
+      cpu_times.reserve(default_event_amount);
+      wall_times.reserve(default_event_amount);
     }
     void TimerWrapper::startTimers()
     {
@@ -13,13 +14,36 @@
     {
       cpu_clock_end = std::clock();
       walltime_clock_end = std::chrono::high_resolution_clock::now();
+      
+      cpu_times.push_back((cpu_clock_end - cpu_clock_start));
+      //wall_times.push_back(std::chrono::duration<double, std::milli>(walltime_clock_end-walltime_clock_start).count());
     }
     void TimerWrapper::printInfo(std::ostream& os)
     {
       os << "Event name: " << event_name << "\n";
-      os << std::fixed << std::setprecision(2) << "CPU time used: "
-              << 1000.0 * (cpu_clock_end - cpu_clock_start) / CLOCKS_PER_SEC << " ms\n"
-              << "Wall clock time passed: "
-              << std::chrono::duration<double, std::milli>(walltime_clock_end-walltime_clock_start).count()
-              << " ms\n";      
+      size_t n = cpu_times.size();
+      if(n > 1)
+      {
+        std::clock_t totalTime = 0;
+        for (size_t i = 0; i < n; i++)
+        {
+          totalTime += cpu_times[i];
+        }
+        os << "For a total of " << n << " event samplings:" << "\n";
+        os << std::fixed << std::setprecision(2) << "CPU time used: "
+                << 1000.0 * totalTime / CLOCKS_PER_SEC << " ms\n"
+                //<< "Wall clock time passed: "
+                //<< std::chrono::duration<double, std::milli>(walltime_clock_end-walltime_clock_start).count()
+                //<< " ms\n";
+                ;
+        
+      }
+      else 
+      {
+        os << std::fixed << std::setprecision(2) << "CPU time used: "
+                << 1000.0 * (cpu_clock_end - cpu_clock_start) / CLOCKS_PER_SEC << " ms\n"
+                << "Wall clock time passed: "
+                << std::chrono::duration<double, std::milli>(walltime_clock_end-walltime_clock_start).count()
+                << " ms\n";
+      }
     }
--- TimerWrapper.h
+++ TimerWrapper.h
@@ -4,14 +4,19 @@
 #include <ctime>
 #include <chrono>
 #include <iomanip>
+#include <vector>
 class TimerWrapper
 {
   private:
     std::clock_t cpu_clock_start, cpu_clock_end;
     std::chrono::high_resolution_clock::time_point walltime_clock_start, walltime_clock_end;
+
+    std::vector<std::clock_t> cpu_times;
+    std::vector<std::chrono::duration<double, std::milli>> wall_times;
+
     std::string event_name;
   public:
-    TimerWrapper(const std::string& event);
+    TimerWrapper(const std::string& event, size_t default_event_amount = 100);
     void startTimers();
     void stopTimers();
     void printInfo(std::ostream& os);
